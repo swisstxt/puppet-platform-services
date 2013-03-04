322accd0cd21b27712a8fda9aef5925fbc81d8bb
Simon Josi <simon.josi@swisstxt.ch>
2013-03-04 14:06:24 +0100

    use top-scope variables for cloudstack credentials for now

--- platform_services_cloudstack/manifests/controller.pp
+++ platform_services_cloudstack/manifests/controller.pp
@@ -1,8 +1,8 @@
-class platform_services_cloudstack::controller(
+class platform_services_cloudstack::controller {
   class{'::cloudstack::controller':
     url => 'https://cloud.swisstxt.ch/client/api',
-    api_key => '',
-    secret_key => '',
+    api_key => $::cloudstack_api_key,
+    secret_key => $::cloudstack_secret_key,
     project => inline_template("<%= scope.lookupvar('::ue').capitalize %>_<%= scope.lookupvar('::project').capitalize %>"),
   }
   Cloudstack_port_forwarding <<||>>
