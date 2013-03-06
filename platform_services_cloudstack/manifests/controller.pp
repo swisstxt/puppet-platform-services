b6499b91c782214dd0b3eefeaa7d26d183536d8f
Simon Josi <simon.josi@swisstxt.ch>
2013-03-06 12:10:38 +0100

    cloudstack api credentials: project is all uppercase

--- platform_services_cloudstack/manifests/controller.pp
+++ platform_services_cloudstack/manifests/controller.pp
@@ -3,7 +3,7 @@ class platform_services_cloudstack::controller {
     url => 'https://cloud.swisstxt.ch/client/api',
     api_key => $::cloudstack_api_key,
     secret_key => $::cloudstack_secret_key,
-    project => inline_template("<%= scope.lookupvar('::ue').capitalize %>_<%= scope.lookupvar('::project').capitalize %>"),
+    project => inline_template("<%= scope.lookupvar('::ue').upcase %>_<%= scope.lookupvar('::project').upcase %>"),
   }
   Cloudstack_port_forwarding <<||>>
 }
