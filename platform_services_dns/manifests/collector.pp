1e8204725fbc6f2f2c7ea49466890f1ba085fb4d
Simon Josi <simon.josi@swisstxt.ch>
2013-03-06 14:38:13 +0100

    dns: fix exported resources collector

--- platform_services_dns/manifests/collector.pp
+++ platform_services_dns/manifests/collector.pp
@@ -1,7 +1,7 @@
 class platform_services_dns::collector {
-  Site_dns::Member::Zone::Export <<||>> {
+  Platform_services_dns::Member::Zone::Export <<||>> {
     require => Class['dns::server'],
     notify  => Class['dns::server::service']
   }
-  Site_dns::Server::Zone <<||>>
+  Platform_services_dns::Server::Zone <<||>>
 }
