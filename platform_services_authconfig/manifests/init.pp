40aeb8e0265232f7d7e78e957c9f9c957011f6da
Simon Josi <simon.josi@swisstxt.ch>
2013-04-23 03:31:46 -0400

    set more default for authconfig module

--- platform_services_authconfig/manifests/init.pp
+++ platform_services_authconfig/manifests/init.pp
@@ -6,6 +6,9 @@ class platform_services_authconfig {
     sssd => hiera('sssd', true),
     sssdauth => hiera('sssdauth', true),
     mkhomedir => hiera('mkhomedir', true),
-    ldaploadcacert => hiera('ldaploadcacert', 'https://gist.github.com/anonymous/5388220/raw/e4ab3a59fb5459ca74e72a8ae74318e4b84151cf/swisstxtCA'),
+    ldaptls => hiera('ldaptls', true),
+    ldapserver => hiera('ldapserver', 'dirsrv01.stxt.mpc'),
+    ldaploadcacert => hiera('ldaploadcacert', 'https://gist.github.com/turingmachine/5433495/raw/b8ca08ecc652266eed3d32aefd8f506d7ea6b722/siwsstxtCA'),
+    sssd_enumerate => hiera('sssd_enumerate', true),
   }
 }
