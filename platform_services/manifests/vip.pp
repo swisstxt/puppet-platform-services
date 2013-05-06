bbda4af8ad065244a393224f58c3cfb629def0a5
Simon Josi <simon.josi@swisstxt.ch>
2013-05-06 09:07:20 -0400

    fix wording

--- platform_services/manifests/vip.pp
+++ platform_services/manifests/vip.pp
@@ -5,7 +5,7 @@ class platform_services::vip(
     fail("must provide top-scope variable \$${::platform_services::node_role}_vips")
   }
   unless has_key($::platform_services::vips, $::platform_services::node_nr) {
-    fail("must provide vip for ${::platform_services::node_role} node_nr ${::platform_services::node_nr}")
+    fail("must provide vip for ${::platform_services::node_role} node number ${::platform_services::node_nr}")
   }
 
   $ip = $::platform_services::vips[$::platform_services::node_nr]
