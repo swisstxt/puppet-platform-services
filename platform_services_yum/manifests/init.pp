b5239ea14f2e26164f1143fd1dad287d9f8c9d88
Simon Josi <simon.josi@swisstxt.ch>
2013-05-13 04:08:52 -0400

    disable yum repo

--- platform_services_yum/manifests/init.pp
+++ platform_services_yum/manifests/init.pp
@@ -10,5 +10,5 @@ class platform_services_yum {
     includepkgs => 'icinga*',
   }
   include ::platform_services_yum::repo::swisstxt
-  include ::platform_services_yum::repo::platform_services
+  #include ::platform_services_yum::repo::platform_services
 }
