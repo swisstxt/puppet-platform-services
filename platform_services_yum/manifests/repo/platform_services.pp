8f20b2ddac6e0731007bcd002131b14a15686004
Simon Josi <simon.josi@swisstxt.ch>
2013-05-13 04:07:24 -0400

    platform-services yum repo: fix naming

--- platform_services_yum/manifests/repo/platform_services.pp
+++ platform_services_yum/manifests/repo/platform_services.pp
@@ -1,5 +1,5 @@
 class platform_services_yum::repo::platform_services {
-  yum::repo{'platform_services':
+  yum::repo{'platform-services':
     descr => 'platform-services Yum Repostory',
     baseurl => "http://${servername}/centos.${lsbmajdistrelease}.${architecture}",
     enabled => 1,
@@ -7,7 +7,7 @@ class platform_services_yum::repo::platform_services {
     gpgcheck => 0,
   } ->
   augeas{'yumrepo: skip_if_unavailable':
-    context => '/files/etc/yum.repos.d/platform_services.repo/platform_services',
+    context => '/files/etc/yum.repos.d/platform-services.repo/platform-services',
     changes => 'set skip_if_unavailable 1',
   } -> Package <| (title != 'ruby-augeas' and title != 'yum-priorities') |>
 }
