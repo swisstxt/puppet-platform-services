6c5a18865ee7e1859eeae5fb24451114a53961da
Simon Josi <simon.josi@swisstxt.ch>
2013-06-10 16:03:02 +0200

    fix mpc_zone fact

--- platform_services/lib/facter/mpc_zone.rb
+++ platform_services/lib/facter/mpc_zone.rb
@@ -1,12 +1,7 @@
 Facter.add("mpc_zone") do
+  domain = Facter.value('domain')
+  mpc_bu = domain[/(\w+)\.\w+\.\w+\.\w+\.\w+$/, 1]
   setcode do
-    case Facter.value('availability_zone')
-    when 'ZUERICH_IX'
-      'zrh'
-    when 'BIEL_CU01'
-      'bie'
-    else
-      'undef'
-    end
+    mpc_bu
   end
 end
