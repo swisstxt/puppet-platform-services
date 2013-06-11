09b4dda9e9ad412dfdee99af3ea65ef121635ab2
Simon Josi <simon.josi@swisstxt.ch>
2013-06-11 10:24:19 +0200

    mv cloudstack_api_key and cloudstack_secret_key out of top-scope params.pp file

--- platform_services_cloudstack/manifests/controller.pp
+++ platform_services_cloudstack/manifests/controller.pp
@@ -1,9 +1,14 @@
 class platform_services_cloudstack::controller {
-  class{'::cloudstack::controller':
-    url => 'https://cloud.swisstxt.ch/client/api',
-    api_key => $::cloudstack_api_key,
-    secret_key => $::cloudstack_secret_key,
-    project => inline_template("<%= scope.lookupvar('::mpc_bu').upcase %>_<%= scope.lookupvar('::mpc_project').upcase %>"),
+  $cloudstack_api_key = hiera('cloudstack_api_key', false)
+  $cloudstack_secret_key = hiera('cloudstack_secret_key', false)
+
+  if $cloudstack_api_key and $cloudstack_secret_key {
+    class{'::cloudstack::controller':
+      url => 'https://cloud.swisstxt.ch/client/api',
+      api_key => $cloudstack_api_key,
+      secret_key => $cloudstack_secret_key,
+      project => inline_template("<%= scope.lookupvar('::mpc_bu').upcase %>_<%= scope.lookupvar('::mpc_project').upcase %>"),
+    }
+    Cloudstack_port_forwarding <<||>>
   }
-  Cloudstack_port_forwarding <<||>>
 }
