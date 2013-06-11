cd5072971e6ec3c4c4fd1c143730d675529444e4
Simon Josi <simon.josi@swisstxt.ch>
2013-06-11 18:39:02 +0200

    fix graylog2 vip port

--- platform_services_graylog2/manifests/server.pp
+++ platform_services_graylog2/manifests/server.pp
@@ -3,6 +3,6 @@ class platform_services_graylog2::server {
   include ::graylog2::web
   include ::platform_services_firewall::http
   class{'::platform_services::vip':
-    ports => 8140,
+    ports => 80,
   }
 }
