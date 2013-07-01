7b4291cffd3f07376101be818c854475674ca0cb
Simon Josi <simon.josi@swisstxt.ch>
2013-07-01 17:36:42 +0200

    haproxy: add support for customer defined additional classes

--- platform_services_haproxy/manifests/server.pp
+++ platform_services_haproxy/manifests/server.pp
@@ -1,3 +1,8 @@
-class platform_services_haproxy::server {
+class platform_services_haproxy::server(
+  $site_classes = undef
+) {
   class{'::haproxy':}
+  if $site_classes {
+    class{$site_classes:}
+  }
 }
