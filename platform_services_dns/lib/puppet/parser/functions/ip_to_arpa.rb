9cd476db350ecf7a0c9caaa4f72eb08c2fffe475
Martin Kolly <martin.kolly@swisstxt.ch>
2013-07-04 16:56:41 +0200

    fix function ip to arpa

--- platform_services_dns/lib/puppet/parser/functions/ip_to_arpa.rb
+++ platform_services_dns/lib/puppet/parser/functions/ip_to_arpa.rb
@@ -1,9 +1,12 @@
 require 'ipaddr'
+
+def convert(ip)
+  IPAddr.new(ip).reverse.split('.')[1..-1].join('.')
+end
+
+
 module Puppet::Parser::Functions
   newfunction(:ip_to_arpa, :type => :rvalue) do |args|
-    def convert(ip)
-      IPAddr.new(ip).reverse.split('.')[1..-1].join('.')
-    end
     if args[0].kind_of?(Array)
       args[0].collect{|ip| convert ip }
     else
