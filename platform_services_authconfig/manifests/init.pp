b22c4fc990b68bfc35d6d731d884f63831c3b151
Andreas Zuber <zuber@puzzle.ch>
2013-07-08 16:53:19 +0200

    deliver ldap cacert with puppet

--- platform_services_authconfig/manifests/init.pp
+++ platform_services_authconfig/manifests/init.pp
@@ -1,14 +1,25 @@
 class platform_services_authconfig {
+
   host{'dirsrv01.stxt.mpc':
     ip => '10.100.219.85',
   } ~>
+  file{'/etc/openldap/certs/cacert.pem':
+    source => [
+      "puppet:///modules/site_authconfig/cacerts/${::fqdn}/cacert.pem",
+      'puppet:///modules/site_authconfig/cacerts/cacert.pem',
+      'puppet:///modules/platform_services_authconfig/cacerts/cacert.pem'
+    ],
+    owner  => root,
+    group  => 0,
+    mode   => '0644';
+  } ~>
   class{'authconfig':
-    sssd => hiera('sssd', true),
-    sssdauth => hiera('sssdauth', true),
-    mkhomedir => hiera('mkhomedir', true),
-    ldaptls => hiera('ldaptls', true),
-    ldapserver => hiera('ldapserver', 'dirsrv01.stxt.mpc'),
-    ldaploadcacert => hiera('ldaploadcacert', 'https://gist.github.com/turingmachine/5433495/raw/b8ca08ecc652266eed3d32aefd8f506d7ea6b722/siwsstxtCA'),
+    sssd           => hiera('sssd', true),
+    sssdauth       => hiera('sssdauth', true),
+    mkhomedir      => hiera('mkhomedir', true),
+    ldaptls        => hiera('ldaptls', true),
+    ldapserver     => hiera('ldapserver', 'dirsrv01.stxt.mpc'),
+    ldaploadcacert => hiera('ldaploadcacert', 'file:///etc/openldap/certs/cacert.pem'),
     sssd_enumerate => hiera('sssd_enumerate', true),
   }
 }
