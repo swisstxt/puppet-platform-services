38c308a09a0523c3be7cb0e42bff62f5c0e87d3c
Rene Moser <rene.moser@swisstxt.ch>
2013-07-24 14:03:26 +0200

    added new hiera variable $cloudstack_url, so our test cloud can be configured

--- platform_services_cloudstack/manifests/controller.pp
+++ platform_services_cloudstack/manifests/controller.pp
@@ -1,13 +1,14 @@
 class platform_services_cloudstack::controller {
-  $cloudstack_api_key = hiera('cloudstack_api_key', false)
+  $cloudstack_api_key    = hiera('cloudstack_api_key', false)
   $cloudstack_secret_key = hiera('cloudstack_secret_key', false)
+  $cloudstack_url        = hiera('cloudstack_url', 'https://cloud.swisstxt.ch/client/api')
 
   if $cloudstack_api_key and $cloudstack_secret_key {
     class{'::cloudstack::controller':
-      url => 'https://cloud.swisstxt.ch/client/api',
-      api_key => $cloudstack_api_key,
+      url        => $cloudstack_url,
+      api_key    => $cloudstack_api_key,
       secret_key => $cloudstack_secret_key,
-      project => inline_template("<%= scope.lookupvar('::mpc_bu').upcase %>_<%= scope.lookupvar('::mpc_project').upcase %>"),
+      project    => inline_template("<%= scope.lookupvar('::mpc_bu').upcase %>_<%= scope.lookupvar('::mpc_project').upcase %>"),
     }
     Cloudstack_port_forwarding <<||>>
   }
