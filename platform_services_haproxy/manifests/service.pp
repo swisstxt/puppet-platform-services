a913c53f3e380b37370c44083b678808b1add705
Rene Moser <rene.moser@swisstxt.ch>
2013-08-08 18:31:23 +0200

    refactored keepalived haproxy setup

--- platform_services_haproxy/manifests/service.pp
+++ platform_services_haproxy/manifests/service.pp
@@ -2,22 +2,23 @@ define platform_services_haproxy::service(
   $ipaddress,
   $ports,
   $options = {},
-  $high_available = false,
+  $virtual_router_id = undef
 ) {
-  if ($high_available) {
-    sysctl::value{'net.ipv4.ip_nonlocal_bind': value => '1'}
-
-    include keepalived
-    keepalived::instance{$name:
-      interface   => 'eth0',
-      virtual_ips => [ $ipaddress ],
-      state       => 'MASTER',
-      priority    => 1,
-    }
-    keepalived::vrrp_script{'haproxy': name_is_proces => true }
+  if $::platform_services_haproxy::server::high_available {
+    if ! $virtual_router_id {
+        fail("virtual_router_id must be defined if high available")
+    } else {
+      keepalived::instance{$virtual_router_id:
+        interface    => 'eth0',
+        virtual_ips  => [ "$ipaddress/24" ],
+        state        => 'MASTER',
+        priority     => 1,
+        track_script => [ "haproxy" ],
+        auth_type    => "PASS",
+        auth_pass    => "635178udDK1AQ123",
+      }
     }
   }
-
   haproxy::listen{$name:
     ipaddress => $ipaddress,
     ports     => $ports,
