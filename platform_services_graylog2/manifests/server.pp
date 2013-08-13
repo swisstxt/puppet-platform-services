a9ca260b1fbae471ecd5d98ced611f997c70e311
Rene Moser <rene.moser@swisstxt.ch>
2013-08-13 17:18:24 +0200

    added firewall rule open 514 udp for syslog

--- platform_services_graylog2/manifests/server.pp
+++ platform_services_graylog2/manifests/server.pp
@@ -2,5 +2,6 @@ class platform_services_graylog2::server {
   include ::graylog2::server
   include ::graylog2::web
   include ::platform_services_firewall::http
+  include ::platform_services_firewall::syslog
   include ::platform_services::front_ip
 }
