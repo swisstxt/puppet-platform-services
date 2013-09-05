swisstxt/puppet-platform-services

commit a25090a5f471401df49cee6918db9cbeb7677636

splitted nameserver in front and internal, used for haproxy in front network

platform_services_dns/manifests/server.pp
@@ -1,5 +1,6 @@
 class platform_services_dns::server {
-  require platform_services_dns
+  include ::platform_services_dns::collector
+  include ::platform_services_firewall::dns
 
   # fail if interfaces are not available
   unless (is_mac_address($::platform_services_dns::macaddress_serv) and is_mac_address($::platform_services_dns::macaddress_sync) and is_mac_address($platform_services_dns::macaddress_stor)) {
@@ -12,36 +13,31 @@ class platform_services_dns::server {
     include ::platform_services_firewall::dns
     include ::dns
 
-    class{'::platform_services::front_ip':
-      ports => 53,
-    } ->
-    platform_services_cloudstack::port_forwarding{'53/udp':
-      front_ip => $::platform_services::front_ip::ip,
-      port => 53,
-      protocol => 'udp',
-    }
-
-    class{'::platform_services_resolvconf::nameserver':
-      ip => $::platform_services_dns::ipaddress_serv,
-    }
-
-    platform_services_dns::server::zone{
-      "${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":
-        nsip => $::platform_services_dns::ipaddress_serv;
-      "${::mpc_zone}.sync.${::mpc_project}.${::mpc_bu}.mpc":
-        nsip => $::platform_services_dns::ipaddress_sync;
-      "${::mpc_zone}.stor.${::mpc_project}.${::mpc_bu}.mpc":
-        nsip => $::platform_services_dns::ipaddress_stor;
-    }
-    
-    if $::platform_services::manage_front_ips {
-      platform_services_dns::server::zone{
-        "${::mpc_project}.${::mpc_bu}.mpc":
-          nsip => $::platform_services::front_ip::ip,
-          rdns_networks => $::mpc_network_front, 
-      }
-    }
-  } else {
-    warning("dns server must have all interfaces up and running")
+  class{'::platform_services_resolvconf::nameserver':
+    vip => $vip,
+  }
+  class{'dns':
+    named_conf_template => 'platform_services_dns/named.conf.erb',
+  }
+  @@platform_services_dns::server::zone{
+    "${::mpc_project}.${::mpc_bu}.mpc":
+      nsip => $vip,
+      rdns_networks => $::mpc_network_front;
+    "${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":
+      nsip => $::ipaddress_eth0;
+    "${::mpc_zone}.sync.${::mpc_project}.${::mpc_bu}.mpc":
+      nsip => $::ipaddress_eth1;
+    "${::mpc_zone}.stor.${::mpc_project}.${::mpc_bu}.mpc":
+      nsip => $::ipaddress_eth2;
+  }
+  platform_services_cloudstack::port_forwarding{'53/tcp':
+    vip =>  $vip,
+    port => 53,
+    protocol => 'tcp',
+  }
+  platform_services_cloudstack::port_forwarding{'53/udp':
+    vip =>  $vip,
+    port => 53,
+    protocol => 'udp',
   }
 }

platform_services_resolvconf/manifests/init.pp
@@ -1,5 +1,9 @@
 class platform_services_resolvconf {
   resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
-  resolvconf::search{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
-  Resolvconf::Nameserver <<||>>
+  #resolvconf::search{$domain:}
+  if $::mpc_network_front == $::network_eth0 { 
+    Resolvconf::Nameserver <<|tag=='front'|>>
+  } else {
+   Resolvconf::Nameserver <<|tag=='internal'|>>
+  }
 }

platform_services_resolvconf/manifests/nameserver.pp
@@ -1,14 +1,18 @@
 class platform_services_resolvconf::nameserver(
-  $ip,
-  $default_nameserver = regsubst($ip, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1'),
+  $vip,
 ) {
   Class['::platform_services_resolvconf::nameserver'] <- Class['::dns::server::service']
-  
-
-  @@resolvconf::nameserver{$ip:
-    ensure => present,
-  }  
+  @@resolvconf::nameserver{$vip:
+    priority => 1,
+    tag => 'front',
+  }
+  @@resolvconf::nameserver{$::ipaddress:
+    priority => 1,
+    tag => 'internal'
+  }
+  $default_nameserver = regsubst($::ipaddress, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
   @@resolvconf::nameserver{$default_nameserver:
-    ensure => absent,
+    priority => 10,
+    tag => 'internal',
   }
 }

commit 6aa2d5e4798576a2f016e6950668a564fb5f94aa

fixed merge a25090a5f471401df49cee6918db9cbeb7677636 broke dns

platform_services_dns/manifests/server.pp
@@ -1,6 +1,5 @@
 class platform_services_dns::server {
-  include ::platform_services_dns::collector
-  include ::platform_services_firewall::dns
+  require platform_services_dns
 
   # fail if interfaces are not available
   unless (is_mac_address($::platform_services_dns::macaddress_serv) and is_mac_address($::platform_services_dns::macaddress_sync) and is_mac_address($platform_services_dns::macaddress_stor)) {
@@ -13,31 +12,36 @@ class platform_services_dns::server {
     include ::platform_services_firewall::dns
     include ::dns
 
-  class{'::platform_services_resolvconf::nameserver':
-    vip => $vip,
-  }
-  class{'dns':
-    named_conf_template => 'platform_services_dns/named.conf.erb',
-  }
-  @@platform_services_dns::server::zone{
-    "${::mpc_project}.${::mpc_bu}.mpc":
-      nsip => $vip,
-      rdns_networks => $::mpc_network_front;
-    "${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":
-      nsip => $::ipaddress_eth0;
-    "${::mpc_zone}.sync.${::mpc_project}.${::mpc_bu}.mpc":
-      nsip => $::ipaddress_eth1;
-    "${::mpc_zone}.stor.${::mpc_project}.${::mpc_bu}.mpc":
-      nsip => $::ipaddress_eth2;
-  }
-  platform_services_cloudstack::port_forwarding{'53/tcp':
-    vip =>  $vip,
-    port => 53,
-    protocol => 'tcp',
-  }
-  platform_services_cloudstack::port_forwarding{'53/udp':
-    vip =>  $vip,
-    port => 53,
-    protocol => 'udp',
+    class{'::platform_services_resolvconf::nameserver':
+      vip => $vip,
+    }
+
+    class{'::platform_services::front_ip':
+      ports => 53,
+    } ->
+    platform_services_cloudstack::port_forwarding{'53/udp':
+      front_ip => $::platform_services::front_ip::ip,
+      port => 53,
+      protocol => 'udp',
+    }
+
+    platform_services_dns::server::zone{
+      "${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":
+        nsip => $::platform_services_dns::ipaddress_serv;
+      "${::mpc_zone}.sync.${::mpc_project}.${::mpc_bu}.mpc":
+        nsip => $::platform_services_dns::ipaddress_sync;
+      "${::mpc_zone}.stor.${::mpc_project}.${::mpc_bu}.mpc":
+        nsip => $::platform_services_dns::ipaddress_stor;
+    }
+    
+    if $::platform_services::manage_front_ips {
+      platform_services_dns::server::zone{
+        "${::mpc_project}.${::mpc_bu}.mpc":
+          nsip => $::platform_services::front_ip::ip,
+          rdns_networks => $::mpc_network_front, 
+      }
+    }
+  } else {
+    warning("dns server must have all interfaces up and running")
   }
 }