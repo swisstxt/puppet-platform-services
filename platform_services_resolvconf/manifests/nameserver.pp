b41b9a57d2752d757088637d0fbe24de31b5ee2a
andreas.wirth@srf.ch <andreas.wirth@srf.ch>
2013-09-20 11:42:16 +0200

    avoid duplicate resource declaration if $default_nameserver is equals to the current node-ip

--- platform_services_resolvconf/manifests/nameserver.pp
+++ platform_services_resolvconf/manifests/nameserver.pp
@@ -1,6 +1,7 @@
 class platform_services_resolvconf::nameserver(
   $vip,
 ) {
+
   Class['::platform_services_resolvconf::nameserver'] <- Class['::dns::server::service']
   @@resolvconf::nameserver{$vip:
     priority => 1,
@@ -18,9 +19,11 @@ class platform_services_resolvconf::nameserver(
     }
   } else {
     $default_nameserver = regsubst($::ipaddress, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
-    @@resolvconf::nameserver{$default_nameserver:
-      priority => 10,
-      tag => 'internal',
+    if $default_nameserver != $::ipaddress {
+      @@resolvconf::nameserver{$default_nameserver:
+        priority => 10,
+        tag => 'internal',
+      }
     }
   }
 }
