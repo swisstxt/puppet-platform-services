18b9be8eaa36ca0b125c43fccf00b79ea7199d4d
andreas.wirth@srf.ch <andreas.wirth@srf.ch>
2013-09-20 14:29:44 +0200

    version of platform_services_resolvconf that works for the moment

--- platform_services_resolvconf/manifests/init.pp
+++ platform_services_resolvconf/manifests/init.pp
@@ -1,9 +1,13 @@
+# Version von platform_services_resolvconf welche für den Moment funktioniert.
+# Die Swisstxt Nameserver - inklusive derjenige von Cloudstack' - delegieren momentan nicht
+# an unsere Server.
+# Um irgendwie weiter arbeiten zu können: Diese Lösung!
+# @see  platform_services_resolvconf::nameserver
+
 class platform_services_resolvconf {
   resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
   #resolvconf::search{$domain:}
-  if $::mpc_network_front == $::network_eth0 { 
-    Resolvconf::Nameserver <<|tag=='front'|>>
-  } else {
-   Resolvconf::Nameserver <<|tag=='internal'|>>
-  }
+
+  Resolvconf::Nameserver <<||>>
+
 }
--- platform_services_resolvconf/manifests/nameserver.pp
+++ platform_services_resolvconf/manifests/nameserver.pp
@@ -1,29 +1,37 @@
-class platform_services_resolvconf::nameserver(
-  $vip,
-) {
+# Version von platform_services_resolvconf::nameserver welche für den Moment funktioniert.
+# Die Swisstxt Nameserver - inklusive derjenige von Cloudstack' - delegieren momentan nicht
+# an unsere Server.
+# Um irgendwie weiter arbeiten zu können: Diese Lösung!
+
+class platform_services_resolvconf::nameserver( $vip,
+                                              )
+{
+
+
+  $default_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
+  if $default_nameserver != $::ipaddress {
+      @@resolvconf::nameserver{$default_nameserver:
+        ensure   => absent,
+        priority => 10,
+        tag      => 'internal',
+      }
+  }
 
   Class['::platform_services_resolvconf::nameserver'] <- Class['::dns::server::service']
-  @@resolvconf::nameserver{$vip:
-    priority => 1,
-    tag => 'front',
-  }
   @@resolvconf::nameserver{$::ipaddress:
     priority => 1,
-    tag => 'internal'
   }
 
-  if $::platform_services_dns::ipaddress_dns_2nd {
-    @@resolvconf::nameserver{$::platform_services_dns::ipaddress_dns_2nd:
-      priority => 2,
-      tag => 'internal',
-    }
-  } else {
-    $default_nameserver = regsubst($::ipaddress, '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
-    if $default_nameserver != $::ipaddress {
-      @@resolvconf::nameserver{$default_nameserver:
-        priority => 10,
-        tag => 'internal',
-      }
-    }
+  @@resolvconf::nameserver{'193.218.104.190':
+    ensure   => absent,
+  }
+
+  @@resolvconf::nameserver{'193.218.103.253':
+    ensure   => absent,
+  }
+
+  @@resolvconf::nameserver{'8.8.8.8':
+    ensure   => present,
+    priority => 2,
   }
 }
