6f3f370cb01aeab31172ec16e94301af141075bd
Rene Moser <rene.moser@swisstxt.ch>
2013-09-20 13:41:34 +0200

    dns: added front dns zone without cs zone

--- platform_services_dns/manifests/server/zone.pp
+++ platform_services_dns/manifests/server/zone.pp
@@ -9,11 +9,13 @@ define platform_services_dns::server::zone(
     soa_email => "admin.$name",
     nameservers => $nameservers,
   }
-  $rdns_zones = ip_to_arpa($rdns_networks, '22')
-  dns::zone{$rdns_zones:
-    serial => 1360059950,
-    soa => "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
-    soa_email => "admin.$name",
-    nameservers => $nameservers,
+  unless $rdns_networks {
+    $rdns_zones = ip_to_arpa($rdns_networks, '22')
+    dns::zone{$rdns_zones:
+      serial => 1360059950,
+      soa => "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
+      soa_email => "admin.$name",
+      nameservers => $nameservers,
+    }
   }
 }
