2b3c79396dc3ce5658e86e5ef9cd77e17a7a9e90
Rene Moser <rene.moser@swisstxt.ch>
2013-09-20 11:58:15 +0200

    dns: changed front zone name

--- platform_services_dns/manifests/server.pp
+++ platform_services_dns/manifests/server.pp
@@ -32,7 +32,7 @@ class platform_services_dns::server {
     
     if $::platform_services::manage_front_ips {
       platform_services_dns::server::zone{
-        "${::mpc_project}.${::mpc_bu}.mpc":
+        "${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":
           nsip => $::platform_services::front_ip::ip,
           rdns_networks => $::mpc_network_front, 
       }
