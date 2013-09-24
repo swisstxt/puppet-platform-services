216614f8f44b5bbac7a3a1350a2170700f7c6ddb
andreas.wirth@srf.ch <andreas.wirth@srf.ch>
2013-09-24 08:59:16 +0200

    reverted some changes in platform_services_resolvconf to check if Swisstext DNS setup actually works

--- platform_services_resolvconf/manifests/init.pp
+++ platform_services_resolvconf/manifests/init.pp
@@ -7,7 +7,10 @@
 class platform_services_resolvconf {
   resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
   #resolvconf::search{$domain:}
+  if $::mpc_network_front == $::network_eth0 {
+    Resolvconf::Nameserver <<|tag=='front'|>>
+  } else {
+    Resolvconf::Nameserver <<|tag=='internal'|>>
 
-  Resolvconf::Nameserver <<||>>
-
+  }
 }
--- platform_services_resolvconf/manifests/nameserver.pp
+++ platform_services_resolvconf/manifests/nameserver.pp
@@ -8,37 +8,27 @@ class platform_services_resolvconf::nameserver( $vip,
 {
 
   Class['::platform_services_resolvconf::nameserver'] <- Class['::dns::server::service']
-
-  if $default_nameserver {
-    @@resolvconf::nameserver{$default_nameserver:
-      priority => 1,
-    }
+  @@resolvconf::nameserver{$vip:
+    priority => 1,
+    tag => 'front',
   }
-  else {
+  @@resolvconf::nameserver{$::ipaddress:
+    priority => 1,
+    tag => 'internal'
+  }
+
+  if $::platform_services_dns::ipaddress_dns_2nd {
+    @@resolvconf::nameserver{$::platform_services_dns::ipaddress_dns_2nd:
+      priority => 2,
+      tag => 'internal',
+    }
+  } else {
     $default_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
     if $default_nameserver != $::ipaddress {
       @@resolvconf::nameserver{$default_nameserver:
-        ensure   => absent,
         priority => 10,
-        tag      => 'internal',
+        tag => 'internal',
       }
     }
-  }
-
-  @@resolvconf::nameserver{$::ipaddress:
-    priority => 1,
-  }
-
-  @@resolvconf::nameserver{'193.218.104.190':
-    ensure   => absent,
-  }
-
-  @@resolvconf::nameserver{'193.218.103.253':
-    ensure   => absent,
-  }
-
-  @@resolvconf::nameserver{'8.8.8.8':
-    ensure   => present,
-    priority => 2,
   }
 }
