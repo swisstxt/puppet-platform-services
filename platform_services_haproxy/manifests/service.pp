5ba02a6a8bfb825ac2cc61350a628b9a082e0862
andreas.wirth@srf.ch <andreas.wirth@srf.ch>
2013-09-24 16:12:54 +0200

    bugfix in switch-case statement (syntax)

--- platform_services_haproxy/manifests/service.pp
+++ platform_services_haproxy/manifests/service.pp
@@ -22,29 +22,30 @@ define platform_services_haproxy::service(
   
   # TODO: move this somewhere else...
   case $::mpc_bu {
-  'srf': {
-    network_config { "eth0:$name":
-      ensure    => 'present',
-      method    => 'static',
-      ipaddress => $ipaddress,
-      netmask   => '255.255.252.0',
-      onboot    => 'true',
-    }~>
-    exec {"reload interface eth0:$name":
-      command => "/sbin/ifdown eth0:$name;/sbin/ifup eth0:$name",
-      refreshonly => true,
-    }->
-    haproxy::listen{$name:
-      ipaddress => $ipaddress,
-      ports     => $ports,
-      options   => $options,
+    'srf': {
+      network_config { "eth0:$name":
+        ensure    => 'present',
+        method    => 'static',
+        ipaddress => $ipaddress,
+        netmask   => '255.255.252.0',
+        onboot    => 'true',
+      }~>
+      exec {"reload interface eth0:$name":
+        command => "/sbin/ifdown eth0:$name;/sbin/ifup eth0:$name",
+        refreshonly => true,
+      }->
+      haproxy::listen{$name:
+        ipaddress => $ipaddress,
+        ports     => $ports,
+        options   => $options,
+      }
     }
-  }
-  default: {
-    haproxy::listen{$name:
-      ipaddress => $ipaddress,
-      ports     => $ports,
-      options   => $options,
+    default: {
+      haproxy::listen{$name:
+        ipaddress => $ipaddress,
+        ports     => $ports,
+        options   => $options,
+      }
     }
   }
 
