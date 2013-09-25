108bf7cf045b7ce2f7e1893ffdd91d30d5013f1d
andreas.wirth@srf.ch <andreas.wirth@srf.ch>
2013-09-25 16:19:10 +0200

    fix in haproxy: Interfaces for Services with more than 10 chars did not came up.

--- platform_services_haproxy/manifests/service.pp
+++ platform_services_haproxy/manifests/service.pp
@@ -21,27 +21,28 @@ define platform_services_haproxy::service(
   }
   
   # TODO: move this somewhere else...
+  $shortname  = regsubst($name,'^(.{1,10}).*$','\1')
   case $::mpc_bu {
     'srf': {
-      network_config { "eth0:$name":
+      network_config { "eth0:$shortname":
         ensure    => 'present',
         method    => 'static',
         ipaddress => $ipaddress,
         netmask   => '255.255.252.0',
         onboot    => 'true',
       }~>
-      exec {"reload interface eth0:$name":
-        command => "/sbin/ifdown eth0:$name;/sbin/ifup eth0:$name",
+      exec {"reload interface eth0:$shortname":
+        command => "/sbin/ifdown eth0:$shortname;/sbin/ifup eth0:$shortname",
         refreshonly => true,
       }->
-      haproxy::listen{$name:
+      haproxy::listen{$shortname:
         ipaddress => $ipaddress,
         ports     => $ports,
         options   => $options,
       }
     }
     default: {
-      haproxy::listen{$name:
+      haproxy::listen{$shortname:
         ipaddress => $ipaddress,
         ports     => $ports,
         options   => $options,
