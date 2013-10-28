f8a9e17b40b0101a736a85cb477816b7985a7088
Carafa, Gianni (SRF) <Gianni.Carafa@srf.ch>
2013-10-28 16:20:42 +0100

    fix for local VAgrant Boxes. They match without a Number

--- platform_services_haproxy/manifests/service.pp
+++ platform_services_haproxy/manifests/service.pp
@@ -60,7 +60,7 @@ define platform_services_haproxy::service(
   }
 
   # Match only first haproxy 
-  if $hostname =~ /^haproxy-0*1/ {
+  if $hostname =~ /^haproxy-?0?1$/ {
     platform_services_dns::member::zone{"${name}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":
       domain    => "${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
       hostname  => $name,
