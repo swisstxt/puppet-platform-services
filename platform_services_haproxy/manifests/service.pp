be1e8631d828ceea62df290065721f54883ce382
Daniel Beyer <ymc-dabe@vcs.ymc.ch>
2013-12-04 22:27:24 +0100

    Revert "fixed keepalived configuration"
    We re-do this in an improved way in later commits...
    
    This reverts commit b4d4f1fc80aa99236a27f60fee345a4a4b45ebc3.

--- platform_services_haproxy/manifests/service.pp
+++ platform_services_haproxy/manifests/service.pp
@@ -11,8 +11,8 @@ define platform_services_haproxy::service(
       keepalived::instance{$virtual_router_id:
         interface    => 'eth0',
         virtual_ips  => [ "$ipaddress/$network_netmask" ],
-        state        => 'BACKUP',
-        priority     => 150 - $::platform_services::node_nr * 50,
+        state        => 'MASTER',
+        priority     => 1,
         track_script => [ "haproxy" ],
         auth_type    => "PASS",
         auth_pass    => "635178udDK1AQ123",
@@ -20,8 +20,22 @@ define platform_services_haproxy::service(
     }
   }
   
+  # TODO: move this somewhere else...
+  $aliasname  = ipv4tohex($ipaddress)
   case $::mpc_bu {
     'srf': {
+      network_config { "eth0:${aliasname}":
+        ensure    => 'present',
+        method    => 'static',
+        ipaddress => $ipaddress,
+        netmask   => '255.255.252.0',
+        onboot    => 'true',
+      }->
+      exec {"reload interface eth0:${aliasname}":
+        command => "/sbin/ifdown eth0:${aliasname};/sbin/ifup eth0:${aliasname}",
+        unless  => "ifconfig | grep -q '^eth0:${aliasname} '",
+        returns => [ 0, 1 ],
+      }->
       haproxy::listen{$name:
         ipaddress => $ipaddress,
         ports     => $ports,
