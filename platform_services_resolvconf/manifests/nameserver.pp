eddc68bd1d42c0b70fb78583df1e5e2969f05e80
Simon Josi <simon.josi@swisstxt.ch>
2013-12-06 15:01:07 +0100

    make sure cloudstack nameserver gets lowest priority

--- platform_services_resolvconf/manifests/nameserver.pp
+++ platform_services_resolvconf/manifests/nameserver.pp
@@ -5,6 +5,12 @@ class platform_services_resolvconf::nameserver(
   Class['::platform_services_resolvconf::nameserver'] <- Class['::dns::server::service']
 
   @@resolvconf::nameserver{$default_nameserver:
+    priority => 1,
+    tag => 'internal',
+  }
+
+  $cloudstack_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
+  @@resolvconf::nameserver{$cloudstack_nameserver:
     priority => 10,
     tag => 'internal',
   }
