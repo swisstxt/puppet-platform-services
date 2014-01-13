c423b9a8bfb794c487d7fa1f2a27eb3073b95d24
Daniel Beyer <ymc-dabe@vcs.ymc.ch>
2014-01-13 10:33:53 +0100

    Simplify dealing with $::platform_services::manage_front_ips
    The way $:::platform_services::manage_front_ips has been used in past
    was rather complicated. In fact we only need a way to disable accessing
    cloudstack in our vagrant setup (which SRF uses for local development).

--- platform_services/manifests/nodes.pp
+++ platform_services/manifests/nodes.pp
@@ -5,9 +5,9 @@ class platform_services::nodes {
     unless hiera('platform_services_puppet::master::disable', false) {
       include ::platform_services_puppet::master
     }
-    if $::platform_services::manage_front_ips {
-      include ::platform_services_cloudstack::controller
-    }
+
+    include ::platform_services_cloudstack::controller
+
     if $osfamily == 'RedHat' {
       include ::platform_services_yum::repo::platform_services::server
     }
