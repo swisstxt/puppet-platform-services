072718d45242ff8dd0ea867cf511480247baca9c
Simon Josi <simon.josi@swisstxt.ch>
2014-01-31 13:15:18 +0100

    revamp resolvconf
      * always remove cloudstack nameserver
      * add platform-services nameservers with highest priority
      * add optional fallback_nameservers with lowest priority

--- platform_services_resolvconf/manifests/init.pp
+++ platform_services_resolvconf/manifests/init.pp
@@ -1,26 +1,30 @@
 class platform_services_resolvconf(
-    $timeout = 3,
+  $timeout = 3,
+  $fallback_nameserver = undef,
 ){
   resolvconf::search{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
   resolvconf::search{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
-
+  
   resolvconf::option {'timeout':
     value => $timeout,
   }
-
+  
+  $cloudstaack_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
+  resolvconf::nameserver{$cloudstack_nameserver:
+    ensure => absent,
+  }
+  
   if $::mpc_network_front == $::network_eth0 {
     resolvconf::domain{"${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc":}
-    Resolvconf::Nameserver <<|tag=='front'|>>
+    Resolvconf::Nameserver <<| tag=='front' |>>
   } else {
     resolvconf::domain{"${::mpc_zone}.serv.${::mpc_project}.${::mpc_bu}.mpc":}
-    Resolvconf::Nameserver <<|tag=='internal'|>>
-    $default_nameserver = regsubst(baseip(), '^(\d+)\.(\d+)\.(\d+)\.(\d+)$',  '\1.\2.\3.1')
-    if $default_nameserver != $::ipaddress and
-       !defined(Resolvconf::Nameserver[$default_nameserver]) {
-      resolvconf::nameserver{$default_nameserver:
-        priority => 10,
-        tag => 'internal',
-      }
+    Resolvconf::Nameserver <<| tag=='serv' |>>
+  }
+
+  if $fallback_nameserver {
+    resolvconf::nameserver{$fallback_nameserver:
+      priority => 10,
     }
   }
 }
