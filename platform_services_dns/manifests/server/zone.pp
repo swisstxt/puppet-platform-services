92fff4a8d16a9ecdaf19acf5da5325df1aeec6fa
Daniel Beyer <ymc-dabe@vcs.ymc.ch>
2014-05-22 10:57:40 +0200

    Do no use hard coded values for SOA serial
    A hard coded value prohibits slave DNS servers to do zone transfers.
    Removing the serial here means we will use the defaults from
    forge module dns (which is the current unix timestamp).
    
    Regressions:
    - Each puppet run on the DNS will do changes to the managed resources
    - Each puppet run on the DNS results in a zone transfer
    - Each puppet run on the DNS will at least reload the nameserver

--- platform_services_dns/manifests/server/zone.pp
+++ platform_services_dns/manifests/server/zone.pp
@@ -4,7 +4,6 @@ define platform_services_dns::server::zone(
   $nameservers = [ "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc", "ns3.swisstxt.ch", "ns4.swisstxt.ch" ]
 ) {
   dns::zone{$name:
-    serial => 1360059950,
     soa => "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
     soa_email => "admin.$name",
     nameservers => $nameservers,
@@ -13,7 +12,6 @@ define platform_services_dns::server::zone(
   }
   $rdns_zones = ip_to_arpa($rdns_networks, $::platform_services::networks_netmask)
   dns::zone{$rdns_zones:
-    serial => 1360059950,
     soa => "${::hostname}.${::mpc_zone}.${::mpc_project}.${::mpc_bu}.mpc",
     soa_email => "admin.$name",
     nameservers => $nameservers,
