02da5a05482e3d5dea0025c7a2c8ad4ca7d20d7c
Rene Moser <rene.moser@swisstxt.ch>
2014-07-18 16:49:05 +0200

    authconfig: fix default ca cert and ldap

--- platform_services_authconfig/manifests/init.pp
+++ platform_services_authconfig/manifests/init.pp
@@ -1,13 +1,14 @@
 class platform_services_authconfig {
 
   host{'dirsrv01.stxt.mpc':
-    ip => '10.100.219.85',
+    ensure => absent,
+    ip     => '10.100.219.85',
   } ~>
   file{'/etc/openldap/certs/cacert.pem':
     source => [
       "puppet:///modules/site_authconfig/cacerts/${::fqdn}/cacert.pem",
       'puppet:///modules/site_authconfig/cacerts/cacert.pem',
-      'puppet:///modules/platform_services_authconfig/cacerts/cacert.pem'
+      'puppet:///modules/platform_services_authconfig/cacerts/DigiCertHighAssuranceCA-3.pem'
     ],
     owner  => root,
     group  => 0,
@@ -16,7 +17,7 @@ class platform_services_authconfig {
   class{'authconfig':
     mkhomedir      => hiera('mkhomedir', true),
     ldaptls        => hiera('ldaptls', true),
-    ldapserver     => hiera('ldapserver', 'dirsrv01.stxt.mpc'),
+    ldapserver     => hiera('ldapserver', 'ldap-ha.swisstxt.ch'),
     ldaploadcacert => hiera('ldaploadcacert', 'file:///etc/openldap/certs/cacert.pem'),
     sssd_enumerate => hiera('sssd_enumerate', true),
   }
