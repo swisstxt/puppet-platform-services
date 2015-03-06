861b6eafad9a0643afdc72e26e15660db3ce930d
Arnaud Gauthier <arnaud@agauthier.net>
2015-03-06 11:44:36 +0100

    modifié:         platform_services_vmware/manifests/redhat.pp 	modifié:         platform_services_yum/manifests/init.pp 	platform_services_yum/manifests/repo/vmware-tools.pp

--- platform_services_vmware/manifests/redhat.pp
+++ platform_services_vmware/manifests/redhat.pp
@@ -1,13 +1,7 @@
 class platform_services_vmware::redhat {
   if $::virtual == 'vmware' {
-    package { 'open-vm-tools':
+    package { 'vmware-tools-esx-nox':
       ensure => latest,
-    } ~>
-    service { 'vmtoolsd':
-      ensure => running,
-      enable => true,
-      hasstatus => true,
-      hasrestart => true,
     }
   }
 }
