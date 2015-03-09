a63399e164bc3da5d4cd4e72391f66a69112eda8
Rene Moser <rene.moser@swisstxt.ch>
2015-03-09 17:31:07 +0100

    vmware: add support for official vmware tools as requested by RTS
    use official tools by using param in hiera:
    
        platform_services_vmware::redhat::tools_used: official

--- platform_services_vmware/manifests/redhat.pp
+++ platform_services_vmware/manifests/redhat.pp
@@ -1,13 +1,37 @@
-class platform_services_vmware::redhat {
+class platform_services_vmware::redhat (
+  $tools_used  = 'open',
+  $esx_version = 'latest',
+) {
   if $::virtual == 'vmware' {
-    package { 'open-vm-tools':
-      ensure => latest,
-    } ~>
-    service { 'vmtoolsd':
-      ensure => running,
-      enable => true,
-      hasstatus => true,
-      hasrestart => true,
+    if $tools_used != 'open' {
+
+      package { 'open-vm-tools':
+        ensure => absent,
+      } ->
+      yum::repo{'vmware-tools':
+        descr    => 'VMware Tools',
+        baseurl  => "http://packages.vmware.com/tools/esx/${esx_version}/rhel${::lsbmajdistrelease}/${::architecture}",
+        enabled  => 1,
+        priority => 1,
+        gpgcheck => 0,
+      } ->
+      package { 'vmware-tools-esx-nox':
+        ensure => latest,
+      }
+    # default: install open-vm-tools
+    } else {
+      package { 'vmware-tools-esx-nox':
+        ensure => absent,
+      } ->
+      package { 'open-vm-tools':
+        ensure => latest,
+      } ~>
+      service { 'vmtoolsd':
+        ensure     => running,
+        enable     => true,
+        hasstatus  => true,
+        hasrestart => true,
+      }
     }
   }
 }
