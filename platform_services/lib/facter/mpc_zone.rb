343103ac0b2e45e9d767ed3f4fc81755a67e8a7f
lynxnot <dfanciola@gmail.com>
2017-05-18 21:17:46 +0200

    mpc_zone custom fact now ignores local interfaces

--- platform_services/lib/facter/mpc_zone.rb
+++ platform_services/lib/facter/mpc_zone.rb
@@ -1,13 +1,27 @@
 Facter.add("mpc_zone") do
   setcode do
-    ip = Facter.value('ipaddress')
-    case ip[/^\d+\.(\d+)\.\d+\.\d+$/, 1]
-    when '101'
-      'bie'
-    when '102'
-      'zrh'
-    else
-      'undef'
+    interfaces = Facter.value('interfaces').split(',')
+    # Ignore well known local interfaces
+    excluded_if = ['lo', 'docker.*', 'lxdbr.*', 'virbr.*', 'veth.*' ]
+    excluded_if.each do |ifpattern|
+      interfaces.delete_if { |x| x.match(ifpattern) }
     end
+    # Iterate remaining interfaces until the zone is defined
+    zone = 'undef'
+    interfaces.each do |ifname|
+      ip = Facter.value("ipaddress_#{ifname}")
+      unless ip.nil?
+        case ip[/^\d+\.(\d+)\.\d+\.\d+$/, 1]
+        when '101'
+          zone = 'bie'
+        when '102'
+          zone = 'zrh'
+        end
+      end
+      unless zone == 'undef'
+        break
+      end
+    end
+    zone
   end
 end
